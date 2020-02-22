df48574ce36f0218aec0653020c210fc8cc793b1
Gil Arreola <gildardo-arreola@hotmil.com>
2020-02-22 14:16:11 -0600

    22-feb / Casa hogar

---
+++ FRONT END/Gas.js
@@ -0,0 +1,8 @@
+function suma (){
+  var magna = 21
+  var premium = 20
+  var valor1 = Number (document.getElementById("sumando1").value)
+  var valor2 = 
+  document.getElementById ("resultado").innerHTML = valor1 * valor2
+
+}
