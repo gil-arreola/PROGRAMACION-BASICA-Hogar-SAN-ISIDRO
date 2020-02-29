5293d01ca92399d80aeefbbd6964b25204070e05
Gil Arreola <gildardo-arreola@hotmil.com>
2020-02-29 13:51:14 -0600

    2

---
+++ JavaScript/sesion1-reto1.js
@@ -0,0 +1,32 @@
+var robot = "Robodrink";
+var version = 200;
+var automatico = true;
+var ultra = undefined;
+var supersaiyajin = undefined;
+var caminar = null;
+
+var clientes {
+  cantidad: 3,
+  new_date: new Date("29-02-2020"),
+  function: function(){}
+};
+var bares = ["Cervecerias", "Antros"]
+
+
+var humano;
+var maquina;
+
+
+var 1;
+console.log(i);
+
+for (i =0); i<=100; i++)) {
+if (i===0){ console.log ("Apenasvamos calentando")
+          }
+else{
+console.log("El numero " +i+ " es: par"
+            {
+else{
+console.log("El numero " +i+ " es: impar"
+            }
+var inc=0
